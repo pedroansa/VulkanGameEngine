a940117648048b882d16b41e6326a0efebd51879
Pedro Andrade <pedroandrade_jp@hotmail.com>
2025-09-17 15:28:43 -0300

    Change transformComponent to display 3d models

--- VulkanGameEngine/GameObject.h
+++ VulkanGameEngine/GameObject.h
@@ -2,24 +2,15 @@
 
 #include "Model.h"
 
-// std
 #include <memory>
 
 namespace app {
 
-    struct Transform2dComponent {
-        glm::vec2 translation{};  // (position offset)
-        glm::vec2 scale{ 1.f, 1.f };
-        float rotation;
+    struct TransformComponent {
+        glm::vec3 translation{};  // (position offset)
+        glm::vec3 scale{ 1.f, 1.f, 1.f };
+        glm::vec3 rotation;
 
-        glm::mat2 mat2() {
-            const float s = glm::sin(rotation);
-            const float c = glm::cos(rotation);
-            glm::mat2 rotMatrix{ {c, s}, {-s, c} };
-
-            glm::mat2 scaleMat{ {scale.x, .0f}, {.0f, scale.y} };
-            return rotMatrix * scaleMat;
-        }
     };
 
     class GameObject {
