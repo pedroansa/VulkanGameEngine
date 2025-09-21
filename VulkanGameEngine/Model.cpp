67c383ca8710e6443f5d5c40d41f6e5d4fed2ae3
Pedro Andrade <pedroandrade_jp@hotmail.com>
2025-09-21 13:24:55 -0300

    Add stagin buffer to allow optimization

--- VulkanGameEngine/Model.cpp
+++ VulkanGameEngine/Model.cpp
@@ -43,17 +43,34 @@ void app::Model::createVertexBuffers(const std::vector<Vertex>& vertices)
     vertexCount = static_cast<uint32_t>(vertices.size());
     assert(vertexCount >= 3 && "Vertex count must be at least 3");
     VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+
     appDevice.createBuffer(
         bufferSize,
-        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+        VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
         VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+        stagingBuffer,
+        stagingBufferMemory);
+
+    void* data;
+    vkMapMemory(appDevice.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+    memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+    vkUnmapMemory(appDevice.device(), stagingBufferMemory);
+
+    appDevice.createBuffer(
+        bufferSize,
+        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
         vertexBuffer,
         vertexBufferMemory);
 
-    void* data;
-    vkMapMemory(appDevice.device(), vertexBufferMemory, 0, bufferSize, 0, &data);
-    memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
-    vkUnmapMemory(appDevice.device(), vertexBufferMemory);
+    appDevice.copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+    vkDestroyBuffer(appDevice.device(), stagingBuffer, nullptr);
+    vkFreeMemory(appDevice.device(), stagingBufferMemory, nullptr);
+
+
 }
 
 void app::Model::createIndexBuffers(const std::vector<uint32_t>& indices)
@@ -66,17 +83,33 @@ void app::Model::createIndexBuffers(const std::vector<uint32_t>& indices)
     }
     
     VkDeviceSize bufferSize = sizeof(indices[0]) * indexCount;
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+
     appDevice.createBuffer(
         bufferSize,
-        VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+        VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
         VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+        stagingBuffer,
+        stagingBufferMemory);
+
+    void* data;
+    vkMapMemory(appDevice.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+    memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
+    vkUnmapMemory(appDevice.device(), stagingBufferMemory);
+
+    appDevice.createBuffer(
+        bufferSize,
+        VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, 
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
         indexBuffer,
         indexBufferMemory);
 
-    void* data;
-    vkMapMemory(appDevice.device(), indexBufferMemory, 0, bufferSize, 0, &data);
-    memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
-    vkUnmapMemory(appDevice.device(), indexBufferMemory);
+    appDevice.copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+    vkDestroyBuffer(appDevice.device(), stagingBuffer, nullptr);
+    vkFreeMemory(appDevice.device(), stagingBufferMemory, nullptr);
+
 }
 
 std::vector<VkVertexInputBindingDescription> app::Model::Vertex::getBindingDescriptions()
